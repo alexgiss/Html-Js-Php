af7af09f9bfccf2a9b90e60a1b562de6efab32c8
alexgiss <alexgiss2020@outlook.com>
2022-02-22 19:24:40 -0500

    initial commit

---
+++ index.php
@@ -0,0 +1,219 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta name="description" content="Coca cola es la bebida">
+    <meta name="keywords" content="bebida, gaseosa">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <title>
+        Inicio - Biografia
+    </title>
+    <link rel="shortcut icon" href="img/favicon.png">
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,400;1,900&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/normalize.css">
+    <link rel="stylesheet" href="style.css">
+    <script src="https://kit.fontawesome.com/2ebb6c21ab.js" crossorigin="anonymous"></script>
+</head>
+
+<body>
+    <header>
+        <div class="container">
+            <a href="index.html">
+                <img src="img/logo.svg" class="logo">
+            </a>
+            <nav>
+                <a href="#incio">Inicio</a>
+                <a href="#nosotros">Nosotros</a>
+                <a href="#servicios">Servicios</a>
+                <a href="#galeria">Galeria</a>
+                <a href="#contactenos">Contactenos</a>
+            </nav>
+            <a href="#" class="hamb"><i class="fa-solid fa-bars"></i></a>
+        </div>
+
+    </header>
+    <main>
+        <section id="inicio">
+            <img src="img/bannerprincipal.jpg">
+            <div class="bloque-inicio">
+                <h1>Bienvenidos a Coca Cola </h1>
+                <p>Cualquier lugar en el que apoyas una Coca-Cola, se convierte en una mesa. Hay magia cuando comemos juntos.
+                </p>
+                <a href="#nosotros" class="boton boton-rojo">Ver más</a>
+            </div>
+        </section>
+        <section id="nosotros" class="seccion">
+            <div class="container">
+                <p> Cualquier lugar en el que apoyas una Coca-Cola, se convierte en una mesa. Hay magia cuando comemos juntos.
+                </p>
+            </div>
+        </section>
+        <section id="servicios" class="seccion">
+            <div class="container">
+                <div class="row">
+                    <div class="columna columna-33 columna-mobile-100">
+                        <div class="bloque-servicio">
+                            <div class="bloque-img-servicio cuadrado-perfecto">
+                                <img src="img/servicio1.jpg">
+                            </div>
+                            <div class="bloque-contenido-servicio">
+                                <h3>Servicio 1</h3>
+                                <p>Cualquier lugar en el que apoyas una Coca-Cola, se convierte en una mesa. Hay magia cuando comemos juntos.</p>
+                                <a href="#" class="boton boton-banco">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="columna columna-33 columna-mobile-100">
+                        <div class="bloque-servicio">
+                            <div class="bloque-img-servicio cuadrado-perfecto">
+                                <img src="img/servicio2.jpg">
+                            </div>
+                            <div class="bloque-contenido-servicio">
+                                <h3>Servicio 1</h3>
+                                <p>Cualquier lugar en el que apoyas una Coca-Cola, se convierte en una mesa. Hay magia cuando comemos juntos.</p>
+                                <a href="#" class="boton boton-banco">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="columna columna-33 columna-mobile-100">
+                        <div class="bloque-servicio">
+                            <div class="bloque-img-servicio cuadrado-perfecto">
+                                <img src="img/servicio3.jpg">
+                            </div>
+                            <div class="bloque-contenido-servicio">
+                                <h3>Servicio 1</h3>
+                                <p>Cualquier lugar en el que apoyas una Coca-Cola, se convierte en una mesa. Hay magia cuando comemos juntos.</p>
+                                <a href="#" class="boton boton-banco">Ver más</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="galeria">
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio1.jpg">
+                            <h4>Imagen1</h4>
+                        </div>
+                    </div>
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio2.jpg">
+                            <h4>Imagen2</h4>
+                        </div>
+                    </div>
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio1.jpg">
+                            <h4>Imagen3</h4>
+                        </div>
+                    </div>
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio1.jpg">
+                            <h4>Imagen4</h4>
+                        </div>
+                    </div>
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio1.jpg">
+                            <h4>Imagen5</h4>
+                        </div>
+                    </div>
+                    <div class="columna-33 columna-mobile-50">
+                        <div class="cuadrado-perfecto">
+                            <img src="img/servicio1.jpg">
+                            <h4>Imagen6</h4>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="contactenos" class="seccion">
+            <iframe width="520" height="400" frameborder="0"
+                src="https://maps.google.com/maps?width=520&amp;height=400&amp;hl=en&amp;q=%20Quito+()&amp;t=&amp;z=12&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"></iframe>
+            <div class="container-fluid">
+                <div class="row">
+                    <div class="columna columna-41 columna-mobile-100 empujar-68 empujar-mobile-0 sinpadding-mobile">
+                        <form action="" method="post">
+                            <div class="form-block">
+                                <input type="text" name="nombre" class="form-control" placeholder="Nombre">
+                            </div>
+                            <div class="form-block">
+                                <input type="email" name="email" class="form-control" placeholder="Email">
+                            </div>
+                            <div class="form-block">
+                                <textarea name="mensaje" placeholder="Mensaje"></textarea>
+                            </div>
+                            <div class="form-block bloque-ultimo">
+                                <input type="submit" class="boton boton-negro" value="Enviar">
+                            </div>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+    </main>
+    <footer>
+        <div class="container">
+            <div class="row">
+                <div class="columna columna-25">
+                    <img src="img/logo-blanco.png" class="logo-footer">
+                    <p>Cualquier lugar en el que apoyas una Coca-Cola, se convierte en una mesa. Hay magia cuando comemos juntos.</p>
+                </div>
+                <div class="columna columna-25 columna-mobile-100">
+                    <h3>
+                        Temas relacionados
+                    </h3>
+                    <ul>
+                        <li><a href="#">Tema 1</a></li>
+                        <li><a href="#">Tema 2</a></li>
+                        <li><a href="#">Tema 3</a></li>
+                    </ul>
+                </div>
+                <div class="columna columna-25 columna-mobile-100">
+                    <h3>
+                        Datos de contacto
+                    </h3>
+                    <ul>
+                        <li>agy_17septiembre@hotmail.com</li>
+                        <li>+513989985359</li>
+                        <li>Quito</li>
+                    </ul>
+                </div>
+                <div class="columna columna-25 columna-mobile-100">
+                    <h3>
+                        Redes Sociales
+                    </h3>
+                    <ul class="redes">
+                        <li><a href="#">
+                                <i class="fa-brands fa-facebook"></i>
+                            </a>
+                        </li>
+                        <li><a href="#">
+                                <i class="fa-brands fa-instagram"></i>
+                            </a>
+                        </li>
+                        <li><a href="#">
+                                <i class="fa-brands fa-linkedin"></i>
+                            </a>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="barra-footer">
+            &copy; Derecho Reservados 2020
+        </div>
+    </footer>
+    <script src="js/jquery.js"></script>
+    <script src="js/funciones.js"></script>
+</body>
+
+</html>
